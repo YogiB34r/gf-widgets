f13b136b33d60bb1f7f4e2c0b33d1948d3008aaa
Mladen <mladen.sijan@live.com>
2018-08-09 18:29:26 +0200

    Dodat product slider sa funkcionalnostima
    Dodata Slick biblioteka za infinite slide
    Delimicno stilizovan slider

--- includes/gf-scripts.php
+++ includes/gf-scripts.php
@@ -2,16 +2,18 @@
 
 function gf_add_frontend_scripts(){
     wp_enqueue_style('gf-main-css', plugins_url() . '/gf-widgets/css/gf-widget-main.css');
-//     Font awesome
+    // Font awesome
     wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.1.1/css/all.css');
-//     Widget specific css
+    // Widget specific css
     wp_enqueue_style('gf-social-icons-css', plugins_url() . '/gf-widgets/css/gf-social-icons-widget.css');
-//    wp_enqueue_style('gf-product-slider', plugins_url() . '/gf-widgets/css/gf-product-slider-widget.css');
-
-
-
+    wp_enqueue_style('slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css');
+    wp_enqueue_style('slick-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css');
+    wp_enqueue_style('gf-product-slider-css', plugins_url() . '/gf-widgets/css/gf-product-slider-widget.css');
+    wp_enqueue_script('slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', array('jquery'), 'true');
+    wp_enqueue_script('gf-product-slider-js', plugins_url() . '/gf-widgets/js/gf-product-slider.js', array('jquery'), '1.0.0', 'true');
 }
 add_action('wp_enqueue_scripts', 'gf_add_frontend_scripts');
+
 function gf_add_backend_scripts(){
     wp_enqueue_media();
     wp_enqueue_style('gf-social-icons-admin-css', plugins_url() . '/gf-widgets/css/gf-social-icons-admin.css');
--- js/gf-widget-main.js
+++ js/gf-widget-main.js
@@ -1,105 +1,80 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
+  var custom_uploader;
 
-    var custom_uploader;
-    function clickHandler(event, input, submitButton, target) {
-        event.preventDefault();
-        //Extend the wp.media object
-        custom_uploader = wp.media.frames.file_frame = wp.media({
-            title: 'Upload image',
-            button: {
-                text: 'Upload'
-            },
-            multiple: false
-        });
-        //When a file is selected, grab the URL and set it as the text field's value
-        custom_uploader.on('select', function () {
-            var attachment = custom_uploader.state().get('selection').first().toJSON();
-            input.val(attachment.url);
-            $('#widget-gf_image_slider_widget-2-image_1_link').val(' ');
-            submitButton.prop('disabled', false);
-        });
+  function clickHandler(event, input, submitButton, target) {
+    event.preventDefault();
+    //Extend the wp.media object
+    custom_uploader = wp.media.frames.file_frame = wp.media({
+      title: 'Upload image',
+      button: {
+        text: 'Upload'
+      },
+      multiple: false
+    });
+    //When a file is selected, grab the URL and set it as the text field's value
+    custom_uploader.on('select', function() {
+      var attachment = custom_uploader.state().get('selection').first().toJSON();
+      input.val(attachment.url);
+      $('#widget-gf_image_slider_widget-2-image_1_link').val(' ');
+      submitButton.prop('disabled', false);
+    });
 
-        //If the uploader object has already been created, reopen the dialog
-        if (custom_uploader) {
-            custom_uploader.open();
-            return;
-        }
-
-        //Open the uploader dialog
-        custom_uploader.open();
+    //If the uploader object has already been created, reopen the dialog
+    if (custom_uploader) {
+      custom_uploader.open();
+      return;
     }
 
-    $('.gf-upload-image-1').click(function (e) {
-        clickHandler(e, $('.image_1_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
-    });
+    //Open the uploader dialog
+    custom_uploader.open();
+  }
+
+  $('.gf-upload-image-1').click(function(e) {
+    clickHandler(e, $('.image_1_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
+  });
 
 
-    $('.gf-upload-image-2').click(function (e) {
-        clickHandler(e, $('.image_2_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
-    });
+  $('.gf-upload-image-2').click(function(e) {
+    clickHandler(e, $('.image_2_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
+  });
 
 
-    $('.gf-upload-image-3').click(function (e) {
-        clickHandler(e, $('.image_3_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
-    });
-    $('.gf-upload-image-1').click(function (e) {
-        clickHandler(e, $('.image_1_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
-    });
+  $('.gf-upload-image-3').click(function(e) {
+    clickHandler(e, $('.image_3_value'), $('#widget-gf_image_slider_widget-2-savewidget'));
+  });
+  $('.gf-upload-image-1').click(function(e) {
+    clickHandler(e, $('.image_1_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
+  });
 
 
-    $('.gf-upload-image-2').click(function (e) {
-        clickHandler(e, $('.image_2_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
-    });
+  $('.gf-upload-image-2').click(function(e) {
+    clickHandler(e, $('.image_2_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
+  });
 
 
-    $('.gf-upload-image-3').click(function (e) {
-        clickHandler(e, $('.image_3_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
-    });
+  $('.gf-upload-image-3').click(function(e) {
+    clickHandler(e, $('.image_3_value'), $('#widget-gf_image_slider_widget-3-savewidget'));
+  });
 
-    $('.gf-upload-image-logo').click(function (e) {
-        clickHandler(e, $('.logo-image-value'), $('#widget-gf_custom_logo_widget-3-savewidget'));
-    });
+  $('.gf-upload-image-logo').click(function(e) {
+    clickHandler(e, $('.logo-image-value'), $('#widget-gf_custom_logo_widget-3-savewidget'));
+  });
 });
-
-//Product slider
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-
-
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
-
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
-
 
 $('#carouselExample').carousel({
-    interval: 2000
+  interval: 2000
 });
 
-
-$(document).ready(function() {
-    /* show lightbox when clicking a thumbnail */
-    $('a.thumb').click(function(event){
-        event.preventDefault();
-        var content = $('.modal-body');
-        content.empty();
-        var title = $(this).attr("title");
-        $('.modal-title').html(title);
-        content.html($(this).html());
-        $(".modal-profile").modal({show:true});
+/* show lightbox when clicking a thumbnail */
+  $('a.thumb').click(function(event) {
+    event.preventDefault();
+    var content = $('.modal-body');
+    content.empty();
+    var title = $(this).attr("title");
+    $('.modal-title').html(title);
+    content.html($(this).html());
+    $(".modal-profile").modal({
+      show: true
     });
-
+  });
 });
