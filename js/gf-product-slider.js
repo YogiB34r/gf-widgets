512a40e9c766b7e83ea69e1af0568a76385b072c
djavolak <djavolak@mail.ru>
2018-08-13 14:00:19 +0200

    refactored slider init a bit, not starting if not set

--- js/gf-product-slider.js
+++ js/gf-product-slider.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($) {
+	//don't start on wrong pages
+	if (typeof gfSliderColumnCount !== "undefined") {
+		startSlider('#tabs-0');
+		hookSliderEvents();
+
+		//@Important activate slider after tab is displayed in order to have access to proper width
+		$( "#tabs" ).tabs({
+			activate: function (event, ui) {
+				if (!$(ui.newPanel.selector).hasClass('slick-initialized')) {
+					startSlider(ui.newPanel.selector);
+				}
+			}
+		});
+	}
+	
     function startSlider(selector) {
         $(selector).slick({
             infinite: true,
@@ -9,36 +24,25 @@ jQuery(document).ready(function($) {
             nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
         });
     }
+	
+	function hookSliderEvents() {
+		$('.product-slider__control-prev').click(function (e) {
+  		    e.preventDefault();
+		    $('.slider-inner').each(function(key, value) {
+			    if ($(value).attr('aria-hidden') == 'false') {
+				    $('#' + $(value).attr('id')).slick('slickPrev');
+			    }
+		  });
+		});
 
-    //activate first slider
-    startSlider('#tabs-0');
-
-    //@Important activate slider after tab is displayed in order to have access to proper width
-    $( "#tabs" ).tabs({
-        activate: function (event, ui) {
-            if (!$(ui.newPanel.selector).hasClass('slick-initialized')) {
-                startSlider(ui.newPanel.selector);
-            }
-        }
-    });
-
-
-  $('.product-slider__control-prev').click(function (e) {
-      e.preventDefault();
-      $('.slider-inner').each(function(key, value) {
-          if ($(value).attr('aria-hidden') == 'false') {
-            $('#' + $(value).attr('id')).slick('slickPrev');
-          }
-      });
-  });
-
-  $('.product-slider__control-next').click(function (e) {
-      e.preventDefault();
-      $('.slider-inner').each(function(key, value) {
-          if ($(value).attr('aria-hidden') == 'false') {
-              $('#' + $(value).attr('id')).slick('slickNext');
-          }
-      });
-  });
+		$('.product-slider__control-next').click(function (e) {
+			e.preventDefault();
+			$('.slider-inner').each(function(key, value) {
+				if ($(value).attr('aria-hidden') == 'false') {
+					$('#' + $(value).attr('id')).slick('slickNext');
+				}
+			});
+		});		
+	}
 
 });
