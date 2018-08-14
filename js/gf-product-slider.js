2ef7383252c2d996d5507974cd2d2dd60caa9eeb
Mladen <mladen.sijan@live.com>
2018-08-14 14:04:19 +0200

    Product slider style and responsive, search/woocommerce pages style fix

--- js/gf-product-slider.js
+++ js/gf-product-slider.js
@@ -1,48 +1,70 @@
 jQuery(document).ready(function($) {
-	//don't start on wrong pages
-	if (typeof gfSliderColumnCount !== "undefined") {
-		startSlider('#tabs-0');
-		hookSliderEvents();
+  //don't start on wrong pages
+  if (typeof gfSliderColumnCount !== "undefined") {
+    startSlider('#tabs-0');
+    hookSliderEvents();
 
-		//@Important activate slider after tab is displayed in order to have access to proper width
-		$( "#tabs" ).tabs({
-			activate: function (event, ui) {
-				if (!$(ui.newPanel.selector).hasClass('slick-initialized')) {
-					startSlider(ui.newPanel.selector);
-				}
-			}
-		});
-	}
-	
-    function startSlider(selector) {
-        $(selector).slick({
+    //@Important activate slider after tab is displayed in order to have access to proper width
+    $("#tabs").tabs({
+      activate: function(event, ui) {
+        if (!$(ui.newPanel.selector).hasClass('slick-initialized')) {
+          startSlider(ui.newPanel.selector);
+        }
+      }
+    });
+  }
+
+  function startSlider(selector) {
+    $(selector).slick({
+      infinite: true,
+      slidesToShow: gfSliderColumnCount,
+      slidesToScroll: gfSliderColumnCount,
+      arrows: false,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
             infinite: true,
-            slidesToShow: gfSliderColumnCount,
-            slidesToScroll: gfSliderColumnCount,
-            arrows: true,
-            prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
-            nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
-        });
-    }
-	
-	function hookSliderEvents() {
-		$('.product-slider__control-prev').click(function (e) {
-  		    e.preventDefault();
-		    $('.slider-inner').each(function(key, value) {
-			    if ($(value).attr('aria-hidden') == 'false') {
-				    $('#' + $(value).attr('id')).slick('slickPrev');
-			    }
-		  });
-		});
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+			]
+    });
+  }
 
-		$('.product-slider__control-next').click(function (e) {
-			e.preventDefault();
-			$('.slider-inner').each(function(key, value) {
-				if ($(value).attr('aria-hidden') == 'false') {
-					$('#' + $(value).attr('id')).slick('slickNext');
-				}
-			});
-		});		
-	}
+  function hookSliderEvents() {
+    $('.product-slider__control-prev').click(function(e) {
+      e.preventDefault();
+      $('.slider-inner').each(function(key, value) {
+        if ($(value).attr('aria-hidden') == 'false') {
+          $('#' + $(value).attr('id')).slick('slickPrev');
+        }
+      });
+    });
+
+    $('.product-slider__control-next').click(function(e) {
+      e.preventDefault();
+      $('.slider-inner').each(function(key, value) {
+        if ($(value).attr('aria-hidden') == 'false') {
+          $('#' + $(value).attr('id')).slick('slickNext');
+        }
+      });
+    });
+  }
 
 });
