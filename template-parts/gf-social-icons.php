a48cb095e1cd59434b42a796670388d0f6d08c9e
Mladen <mladen.sijan@live.com>
2018-08-16 14:41:59 +0200

    Product slider and social icons style and responsive

--- template-parts/gf-social-icons.php
+++ template-parts/gf-social-icons.php
@@ -2,10 +2,10 @@
 
 <ul class="social-network social-circle">
     <li><a href="<?=$instance['facebook']?>" class="icoFacebook" title="Facebook"><i class="fab fa-facebook"></i></a>
-        <a href="<?=$instance['facebook']?>">nonstopshop.rs</a>
+        <a href="<?=$instance['facebook']?>" class="social-link">nonstopshop.rs</a>
     </li>
     <li><a href="<?=$instance['instagram']?>" class="icoRss" title="Rss"><i class="fab fa-instagram"></i></a>
-        <a href="<?=$instance['instagram']?>">nonstopshop.rs</a>
+        <a href="<?=$instance['instagram']?>" class="social-link">nonstopshop.rs</a>
     </li>
     <!-- <li><a href="$instance['twitter'] . '" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
      <li><a href="' . $instance['google_plus'] . '" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
