6c37aa0befa52a131513c96902b5ab492e5fb505
Mladen <mladen.sijan@live.com>
2018-08-16 17:15:13 +0200

    Newsletter subtitle removed

--- template-parts/gf-newsletter-widget.php
+++ template-parts/gf-newsletter-widget.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default gf-newsletter gf-newsletter--home">
     <div class="panel-body gf-newsletter__body">
-        <p class="gf-newsletter__title"><b class="gf-newsletter__subtitle"><?= __('prijavom se na listu prvi dobijate naše specijalne ponude na email')?></b></p>
+        <p class="gf-newsletter__title"></p>
         <form class="form-inline gf-newsletter__form" action="" method="post">
             <input type="email" class="form-control input-lg gf-newsletter__input" placeholder="<?= __('Vaš e-mail...')?>">
             <button type="submit" class="btn btn-block gf-newsletter__submit"><?= __('Prijava na listu')?></button>
