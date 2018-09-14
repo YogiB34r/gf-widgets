4cabcfac87250d37e53e088d09aca2d56004e994
Igor Djuric <igor.djuric011@gmail.com>
2018-09-14 12:49:59 +0200

    Add stickers to products in slider

--- template-parts/gf-product-slider-without-tabs.php
+++ template-parts/gf-product-slider-without-tabs.php
@@ -41,6 +41,8 @@ if ($columnCount === 0) {
                 <a href="<?php echo get_permalink($loop->post->ID) ?>"
                    title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
                     <?php woocommerce_show_product_sale_flash($post, $product); ?>
+                    <?php add_stickers_to_products_new();
+                    add_stickers_to_products_soldout()?>
                     <?php if (has_post_thumbnail($loop->post->ID)) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="' . woocommerce_placeholder_img_src() . '" alt="Placeholder" width="300px" height="300px" />'; ?>
                     <h5><?php the_title(); ?></h5>
                     <span class="price"><?php echo $product->get_price_html(); ?></span>
