e538c4c6c6ecd8f4205254c62137b989c4576f29
djavolak <djavolak@mail.ru>
2018-09-14 12:38:29 +0200

    casting counter to int to avoid js errors

--- template-parts/gf-product-slider.php
+++ template-parts/gf-product-slider.php
@@ -12,7 +12,7 @@ if (isset($instance['slider_title']) and !empty($instance['slider_title'])) {
 }
 $columnCount = (int) $instance['number_of_columns'];
 if ($columnCount === 0) {
-    $columnCount = 1;
+    $columnCount = 5;
 }
 ?>
 
