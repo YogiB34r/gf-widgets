faab38fcfe4cf0756864cbb620863c2cd0e23294
Vladimir Jankovic <v.jankovic992@gmail.com>
2018-09-17 14:59:24 +0200

    removed parent cat tab and added custom link for box title

--- template-parts/gf-product-slider.php
+++ template-parts/gf-product-slider.php
@@ -1,22 +1,16 @@
 <?php
+var_dump($instance);
 $random_id = rand();
-$category_term = get_term_by('slug', $instance['category_select'], 'product_cat');
-$category_link = get_term_link($category_term->term_id);
-if (isset($instance['slider_title']) and !empty($instance['slider_title'])) {
-    $slider_title = $instance['slider_title'];
-} else {
-    $slider_title = $category_term->name;
-}
 $columnCount = (int) $instance['number_of_columns'];
 if ($columnCount === 0) {
     $columnCount = 5;
 }
 $itemLimit = 10;
 ?>
-  <div id="<?php echo $random_id; ?>" class="gf-product-slider">
+  <div id="<?php echo $random_id;?>" class="gf-product-slider">
     <div id="tabs">
       <div class="row gf-product-slider__header">
-        <h3 class="gf-product-slider__header__title"><a href="<?= $category_link ?>"><?= $slider_title ?></a></h3>
+        <h3 class="gf-product-slider__header__title"><a href="<?=$instance['title_link']?>"><?= $instance['slider_title'] ?></a></h3>
           <?php if (isset($instance['tab_1']) && !empty(($instance['tab_1'])) ||
                     isset($instance['tab_2']) && !empty(($instance['tab_2'])) ||
                     isset($instance['tab_3']) && !empty(($instance['tab_3'])) ||
@@ -24,9 +18,6 @@ $itemLimit = 10;
                     isset($instance['tab_5']) && !empty(($instance['tab_5']))):?>
           <div class="gf-product-slider__tabs">
           <ul>
-            <li>
-                <a href="#tabs-0">Sve</a>|
-            </li>
             <?php if (isset($instance['tab_1']) and !empty($instance['tab_1'])): ?>
             <li>
                 <a href="#tabs-1"><?= get_term_by('slug', $instance['tab_1'], 'product_cat')->name ?></a>|
@@ -63,33 +54,6 @@ $itemLimit = 10;
               <i class="fas fa-angle-right product-slider__control-next-icon"></i>
             </a>
         </div>
-      </div>
-
-      <div id="tabs-0" class="slider-inner">
-        <?php
-            $args = array('post_type' => 'product', 'posts_per_page' => $itemLimit, 'product_cat' => $instance['category_select'],
-                'orderby' => 'name',
-                'meta_query' => array(
-                    array(
-                        'key' => '_stock_status',
-                        'value' => 'instock'
-                   ),
-            ));
-            $loop = new WP_Query($args);
-            while ($loop->have_posts()) :
-                $loop->the_post();
-                global $product; ?>
-          <div class="slider-item">
-            <a href="<?php echo get_permalink($loop->post->ID) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
-              <?php woocommerce_show_product_sale_flash('', $product); ?>
-              <?php if (has_post_thumbnail($loop->post->ID)) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="' . woocommerce_placeholder_img_src() . '" alt="Placeholder" width="300px" height="300px" />'; ?>
-              <h5><?php the_title(); ?></h5>
-              <span class="price"><?php echo $product->get_price_html(); ?></span>
-            </a>
-            <?php woocommerce_template_loop_add_to_cart($loop->post, $product); ?>
-          </div>
-          <?php endwhile; ?>
-          <?php wp_reset_query(); ?>
       </div>
       <?php if (isset($instance['tab_1']) and !empty($instance['tab_1'])): ?>
       <div id="tabs-1" class="slider-inner">
