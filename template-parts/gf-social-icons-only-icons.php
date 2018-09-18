6e869e8363cc45bf1d29f63558787f60d7d940b9
Vladimir Jankovic <v.jankovic992@gmail.com>
2018-09-18 14:19:09 +0200

    changed template to only show 2 icons instead of box

---
+++ template-parts/gf-social-icons-only-icons.php
@@ -0,0 +1,8 @@
+<ul class="">
+    <li>
+        <a href="<?=$instance['facebook']?>" class="facebook-icon" title="Facebook"><img src="../icons/facebook.png"></a>
+    </li>
+    <li>
+        <a href="<?=$instance['instagram']?>" class="instagram-icon"><img src="../icons/instagram.png" alt=""></a>
+    </li>
+</ul>
--- template-parts/gf-social-icons.php
+++ template-parts/gf-social-icons.php
@@ -1,5 +1,3 @@
-
-
 <ul class="social-network social-circle">
     <li><a href="<?=$instance['facebook']?>" class="icoFacebook" title="Facebook"><i class="fab fa-facebook"></i></a>
         <a href="<?=$instance['facebook']?>" class="social-link">nonstopshop.rs</a>
@@ -7,8 +5,5 @@
     <li><a href="<?=$instance['instagram']?>" class="icoRss" title="Rss"><i class="fab fa-instagram"></i></a>
         <a href="<?=$instance['instagram']?>" class="social-link">nonstopshop.rs</a>
     </li>
-    <!-- <li><a href="$instance['twitter'] . '" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-     <li><a href="' . $instance['google_plus'] . '" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
-     <li><a href="' . $instance['linked_in'] . '" class="icoLinkedin" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
-     -->
+
 </ul>
