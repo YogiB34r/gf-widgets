3c2b4c9ccd33afe664a05e04a9d16b9087152dfc
YogiB34r <v.jankovic992@gmail.com>
2018-09-24 19:34:46 +0200

    edited social icons widgets to only have 2 icons instead of box

--- template-parts/gf-social-icons-only-icons.php
+++ template-parts/gf-social-icons-only-icons.php
@@ -1,8 +1,11 @@
-<ul class="">
-    <li>
-        <a href="<?=$instance['facebook']?>" class="facebook-icon" title="Facebook"><img src="../icons/facebook.png"></a>
+<?php
+$class = 'list-inline gf-social-icons-list';
+if (get_queried_object() && is_product_category()){$class .= ' gf-social-icons-list-category-page';}?>
+<ul class="<?=$class?>">
+    <li class="list-inline-item">
+        <a href="<?=$instance['facebook']?>" class="facebook-icon" title="Facebook"><img src="/wp-content/plugins/gf-widgets/icons/facebook.png" /></a>
     </li>
-    <li>
-        <a href="<?=$instance['instagram']?>" class="instagram-icon"><img src="../icons/instagram.png" alt=""></a>
+    <li class="list-inline-item">
+        <a href="<?=$instance['instagram']?>" class="instagram-icon"><img src="/wp-content/plugins/gf-widgets/icons/instagram.png" /></a>
     </li>
 </ul>
