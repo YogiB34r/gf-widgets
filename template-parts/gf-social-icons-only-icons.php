3033494ff18be2f74e230f6ae62dfe1e29151968
YogiB34r <v.jankovic992@gmail.com>
2018-09-24 19:57:34 +0200

    some changes to css

--- template-parts/gf-social-icons-only-icons.php
+++ template-parts/gf-social-icons-only-icons.php
@@ -1,6 +1,5 @@
 <?php
-$class = 'list-inline gf-social-icons-list';
-if (get_queried_object() && is_product_category()){$class .= ' gf-social-icons-list-category-page';}?>
+$class = 'list-inline gf-social-icons-list';?>
 <ul class="<?=$class?>">
     <li class="list-inline-item">
         <a href="<?=$instance['facebook']?>" class="facebook-icon" title="Facebook"><img src="/wp-content/plugins/gf-widgets/icons/facebook.png" /></a>
